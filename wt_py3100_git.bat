008c0dfd109f7c8bb4bd23a85818f6957385a304
Nikita <wot_98_98@mail.ru>
2026-09-20 10:56:53 +0600

    Update wt_py3100_git.bat

--- wt_py3100_git.bat
+++ wt_py3100_git.bat
@@ -1,5 +1,4 @@
 @echo off
-chcp 65001 >nul
 setlocal EnableExtensions EnableDelayedExpansion
 
 set "REPO=microsoft/terminal"
@@ -12,13 +11,13 @@ echo   Autoinstall Windows Terminal + Python 3.10.0 + Git
 echo ================================================================================
 echo.
 
-@REM Создаем временную папку
+@REM Create temp dir
 if exist "%TEMP_DIR%" (
     rmdir /s /q "%TEMP_DIR%"
 )
 mkdir "%TEMP_DIR%"
 
-@REM Получаем даныне о последнем релизе терминала
+@REM Getting latest wt release data
 <nul set /p "=[1/8] Getting latest release . . ."
 curl -fsSL "https://api.github.com/repos/%REPO%/releases/latest" -o "%TEMP_DIR%\release.json"
 if errorlevel 1 (
@@ -28,7 +27,7 @@ if errorlevel 1 (
 )
 echo  Done
 
-@REM Получаем ссылку на msixbundle файл
+@REM Getting link for msixbundle file
 <nul set /p "=[2/8] Finding MSIX bundle . . ."
 for /f "delims=" %%A in ('powershell.exe -NoProfile -Command "$json = Get-Content -Raw '%TEMP_DIR%\release.json' | ConvertFrom-Json; $json.assets | Where-Object { $_.name -like 'Microsoft.WindowsTerminal_*.msixbundle' } | Select-Object -First 1 -ExpandProperty browser_download_url"') do (set "DOWNLOAD_URL=%%A")
 if not defined DOWNLOAD_URL (
@@ -47,7 +46,7 @@ echo GITHUB: "!GITHUB_URL!"
 echo TEMP:   "!TEMP_DIR!"
 echo.
 
-@REM Скачиваем msixbundle файл во временную папку
+@REM Download msixbundle in temp dir
 <nul set /p "=[3/8] Downloading Windows Terminal . . ."
 curl -fLs "!DOWNLOAD_URL!" -o "%TEMP_DIR%\!FILE_NAME!"
 if errorlevel 1 (
@@ -57,7 +56,7 @@ if errorlevel 1 (
 )
 echo  Done
 
-@REM Устанавливаем терминал
+@REM Install terminal
 <nul set /p "=[4/8] Installing Windows Terminal . . ."
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Add-AppxPackage -Path '%TEMP_DIR%\!FILE_NAME!'"
 if errorlevel 1 (
@@ -73,7 +72,7 @@ echo  Done
 set "PYTHON_URL=https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe"
 set "PYTHON_INSTALLER=%TEMP_DIR%\python-3.10.0-amd64.exe"
 
-@REM Скачиваем python
+@REM Download python
 curl.exe -fLs "%PYTHON_URL%" -o "%PYTHON_INSTALLER%"
 if errorlevel 1 (
     echo.
@@ -82,7 +81,7 @@ if errorlevel 1 (
 )
 echo  Done
 
-@REM Устанавливаем python
+@REM Install python
 <nul set /p "=[6/8] Installing Python 3.10.0 . . ."
 "%PYTHON_INSTALLER%" /quiet InstallAllUsers=1 PrependPath=1 Include_pip=1 Include_test=0
 if errorlevel 1 (
@@ -97,7 +96,7 @@ echo  Done
 set "GIT_URL=https://github.com/git-for-windows/git/releases/latest/download/Git-64-bit.exe"
 set "GIT_INSTALLER=%TEMP_DIR%\Git-64-bit.exe"
 
-@REM Скачиваем GIT
+@REM Download GIT
 curl.exe -fLs "%GIT_URL%" -o "%GIT_INSTALLER%"
 if errorlevel 1 (
     echo.
@@ -106,7 +105,7 @@ if errorlevel 1 (
 )
 echo  Done
 
-@REM Устнавливаем GIT
+@REM Install GIT
 <nul set /p "=[8/8] Installing Git . . ."
 "%GIT_INSTALLER%" /VERYSILENT /NORESTART /NOCANCEL /SP-
 if errorlevel 1 (
@@ -116,7 +115,7 @@ if errorlevel 1 (
 )
 echo  Done
 
-@REM Удаляем временную папку
+@REM Remove temp dir
 rmdir /s /q "%TEMP_DIR%"
 
 echo.
