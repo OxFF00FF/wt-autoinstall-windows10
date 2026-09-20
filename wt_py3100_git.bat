79527cc4980ddda9d205a3e26d905c14f1f67de6
Nikita <wot_98_98@mail.ru>
2026-09-20 10:33:14 +0600

    Update wt_py3100_git.bat for installation script

--- wt_py3100_git.bat
+++ wt_py3100_git.bat
@@ -2,6 +2,8 @@
 chcp 65001 >nul
 setlocal EnableExtensions EnableDelayedExpansion
 
+:: cls && curl -sL https://raw.githubusercontent.com/OxFF00FF/wt-autoinstall-windows10/main/wt_py3100_git.bat -o "%TEMP%\wt_install.bat" && call "%TEMP%\wt_install.bat" && del "%TEMP%\wt_install.bat"
+
 set "REPO=microsoft/terminal"
 set "TEMP_DIR=%TEMP%\windows-terminal-install"
 set "GITHUB_URL=https://github.com/%REPO%/releases"
